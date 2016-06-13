5297c27ad43c3205346f61cabab6bd7ee0af57db
ch <caohui@donson.com.cn>
2016-06-13 17:48:22 +0800

    add

--- gssg/club/chapter3/LogLoss.py
+++ gssg/club/chapter3/LogLoss.py
@@ -1,12 +1,6 @@
 #!/usr/bin/ env python
 # coding=utf-8
 import math
-
-
-# 公式
-# KL(tctr || pctr)=1/impression * [-click*log(pctr)-(impression-click)*log(1-pctr)+const]
-
-
 # 计算LogLoss
 def calcloss(loss, click, pctr):
     for i in range(1, len(click)):
@@ -20,36 +14,30 @@ def calcloss(loss, click, pctr):
 
 
 # 计算auc
-def calcAUC(data):
+def calcAUC(labels,pctr):
     auc = tp = tp_pre = fp = fp_pre = 0.0
-    last_value = data[0]['pctr']
-    for i in range(len(data)):
-        if data[i]['label'] == 1:
+    sortdata = sorted(range(len(pctr)), key=lambda i: pctr[i], reverse=True)
+
+    last_value = pctr[sortdata[0]]
+    for i in range(len(labels)):
+        if labels[sortdata[i]] == 1:
             tp += 1
         else:
             fp += 1
-        if last_value != data[i]['pctr']:
+        if last_value != pctr[sortdata[i]]:
             auc += (tp + tp_pre) * (fp - fp_pre) / 2.0
             tp_pre = tp
             fp_pre = fp
-            last_value = data[i]['pctr']
+            last_value = pctr[sortdata[i]]
         auc += (tp + tp_pre) * (fp - fp_pre) / 2.0
-
     return auc, tp, fp, auc / (tp * fp)
 
 
 if __name__ == "__main__":
     loss = 0
-    clickDate = [0, 1, 1, 1, 0, 0, 1]
-    pctr = [0.005, 0.01, 0.9, 0.03, 0.109, 0.32, 0.73]
-    print calcloss(loss, clickDate, pctr)
+    # 测试数据
+    labels = [1, 1, 0, 1, 1, 0, 1, 0, 1]
+    pctr = [0.68, 0.9, 0.8, 0.3, 0.9, 0.8, 0.5, 0.1, 0.75]
+    print calcloss(loss, labels, pctr)
     print '----------------------------------'
-
-    # true_data = [0, 1, 1, 1, 0, 0, 1]  # 真实值
-    # pctr_data = [1, 0, 1, 1, 0, 1, 1]  # 预测值
-    # data = [{'label': 1, 'pctr': 0.1}, {'label': 0, 'pctr': 0.2}, {'label': 0, 'pctr': 0.3}, {'label': 1, 'pctr': 0.3},
-    #        {'label': 0, 'pctr': 0.5}, {'label': 1, 'pctr': 0.9}, {'label': 1, 'pctr': 1}]
-    data = [{'label': 1, 'pctr': 0.68}, {'label': 1, 'pctr': 0.9}, {'label': 0, 'pctr': 0.8}, {'label': 1, 'pctr': 0.3},
-            {'label': 0, 'pctr': 0.9}, {'label': 0, 'pctr': 0.8}, {'label': 1, 'pctr': 0.1}, {'label': 0, 'pctr': 0.5}, {'label': 0, 'pctr': 0.75}
-            ]
-    print calcAUC(data)
+    print calcAUC(labels,pctr)
