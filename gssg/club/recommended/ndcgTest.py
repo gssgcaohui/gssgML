bb9d130ae15a0417578595c4176d5b6a26592331
Jiawei Yang <64341393@qq.com>
2016-08-14 22:20:51 +0800

    add

--- gssg/club/recommended/ndcgTest.py
+++ gssg/club/recommended/ndcgTest.py
@@ -1,14 +1,14 @@
 #!/usr/bin/ env python
+# encoding:utf-8
 import sys
 import math
 
-
+# 计算dcg
 def dcg(item_list):
     v = 0.0
     for i in range(len(item_list)):
-            v += (math.pow(2,item_list[i][1])-1) / math.log(i+2,2)
+        v += (math.pow(2, item_list[i][1]) - 1) / math.log(i + 2, 2)
     return v
-
 
 def groupBy(data):
     if len(data) == 0:
@@ -33,7 +33,7 @@ def calculateNdcg(data):
     for d in data:
         d_sorted1 = sorted(d, key=lambda x: x[1], reverse=True)
         d_sorted2 = sorted(d, key=lambda x: x[2], reverse=True)
-        ndcg += dcg(d_sorted2)/dcg(d_sorted1)
+        ndcg += dcg(d_sorted2) / dcg(d_sorted1)
     return ndcg / len(data)
 
 
@@ -42,11 +42,12 @@ def readData(file_name):
     data = []
     for line in f:
         seg = line.strip().split(",")
-        data.append(seg[0], float(seg[1]), float(seg[2]))
+        data.append((seg[0], float(seg[1]), float(seg[2])))
     return data
 
 
 if __name__ == "__main__":
-    data = readData(sys.argv[1])
+    data = readData("mapdata.txt")
+    # data = readData(sys.argv[1])
     group_data = groupBy(data)
     print(calculateNdcg(group_data))
